8b65b98dd1002b5fb0070c1cf708f46f6f613f7c
Krem632kreM <krempinatatyana@gmail.com>
2022-01-14 11:29:56 +0400

    ref: Leskin's realized

--- main.js
+++ main.js
@@ -1,13 +1,4 @@
 'use strict'
-
-const btnRegistration = document.getElementById('registration')
-const btnAutorization = document.getElementById('autorization')
-const usersUl = document.querySelector('.users')
-const date =  new Date().getTime()
-const date1 = new Date()
-
-
-let usersList;
 let monthsList = [
   'января',
   'февраля',
@@ -21,6 +12,11 @@ let monthsList = [
   'ноября',
   'декабря'
 ];
+const btnRegistration = document.getElementById('registration')
+const btnAutorization = document.getElementById('autorization')
+const usersUl = document.querySelector('.users')
+const date = new Date().getTime()
+const date1 = new Date()
 
 function msToTime(duration) {
   var milliseconds = parseInt((duration % 1000) / 100),
@@ -29,35 +25,28 @@ function msToTime(duration) {
     hours = Math.floor((duration / (1000 * 60 * 60)) % 24),
 
     hours = (hours < 10) ? "0" + hours : hours;
-    minutes = (minutes < 10) ? "0" + minutes : minutes;
-    seconds = (seconds < 10) ? "0" + seconds : seconds;
+  minutes = (minutes < 10) ? "0" + minutes : minutes;
+  seconds = (seconds < 10) ? "0" + seconds : seconds;
 
   return hours + ":" + minutes + ":" + seconds;
 }
 
+const usersList = JSON.parse(localStorage.getItem("usersList"))
 
-let getUserList = function () {
-  if (JSON.parse(localStorage.getItem("usersList")) === null) {
-    usersList = []
-  } else {
-    usersList = JSON.parse(localStorage.getItem("usersList"))
-  }
-
-  usersList.forEach(function(user){
+let render = function () {
+  usersUl.textContent = ''
+  usersList.forEach(function (user) {
     const ul = document.createElement('li')
     ul.classList.add('user')
-    ul.innerHTML = 'Имя: ' + user.firstname + ', фамилия: ' + user.surname + ', зарегестрирован:'+ user.day  + ' ' + monthsList[user.month] +' '+user.year + ' г. ' + msToTime(user.d) + "<button class='remove'></button>"
-
+    ul.innerHTML = 'Имя: ' + user.firstname + ', фамилия: ' + user.surname + ', зарегестрирован: ' + user.day + ' ' + monthsList[user.month] + ' ' + user.year + ' г. ' + msToTime(user.d) + "<button class='remove'></button>"
     usersUl.append(ul)
 
     const btnRemove = ul.querySelector('.remove')
     btnRemove.addEventListener('click', function () {
       usersList.shift(user)
-        console.log(user)
-        localStorage.setItem("usersList", JSON.stringify(usersList));
-        window.location.reload();
-      })
-      
+      render();
+    })
+    localStorage.setItem("usersList", JSON.stringify(usersList));
   })
 }
 
@@ -78,15 +67,8 @@ btnRegistration.addEventListener('click', function (event) {
   user.day = date1.getDate()
 
   usersList.push(user)
-
-  const ul = document.createElement('li')
-  ul.classList.add('user')
-  ul.innerHTML = 'Имя: ' + user.firstname + ', фамилия: ' + user.surname + ', зарегестрирован: ' + user.day + ' ' + monthsList[user.month] + ' ' + user.year + ' г. '+ msToTime(user.d) + "<button class='remove'></button>"
-  usersUl.append(ul)
-  localStorage.setItem("usersList", JSON.stringify(usersList));
+  render();
 })
-
-
-getUserList();
+render();
 
 console.log(usersList)
